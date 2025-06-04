be9f0b75dafc30db21d8183ca9927908ceb97d14
LilianBoinard <boinard.lilian@outlook.fr>
2025-06-04 15:08:14 +0200

    Implementation de UserRepository

--- src/App/Repository/User/UserRepository.php
+++ src/App/Repository/User/UserRepository.php
@@ -19,14 +19,28 @@ class UserRepository extends AbstractRepository
 
     public function findByUsername(string $username): ?User
     {
-        $stmt = $this->database->prepare("SELECT * FROM {$this->table} WHERE username = :username LIMIT 1");
+        $sql = "SELECT * FROM {$this->table} WHERE username = :username LIMIT 1";
+
+        $stmt = $this->database->prepare($sql);
         $stmt->execute(['username' => $username]);
         $stmt->setFetchMode(PDO::FETCH_CLASS, $this->entityClass);
         $user = $stmt->fetch();
         return $user ?: null;
     }
 
-    public function save(object $entity): bool {
+    public function findByEmail(string $email): ?User
+    {
+        $sql = "SELECT * FROM {$this->table} WHERE email = :email LIMIT 1";
+
+        $stmt = $this->database->prepare($sql);
+        $stmt->execute(['email' => $email]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, $this->entityClass);
+        $user = $stmt->fetch();
+        return $user ?: null;
+    }
+
+    public function save(object $entity): bool
+    {
 
         if (!$entity instanceof User) {
             throw new \InvalidArgumentException('L\'entité doit être une instance de User');
@@ -34,35 +48,58 @@ class UserRepository extends AbstractRepository
 
         if ($entity->id) {
             // Mise à jour
-            $stmt = $this->database->prepare("
+
+            $sql = "
                         UPDATE {$this->table} 
                         SET username = :username, password = :passowrd, email = :email
-                        WHERE id = :id
-                        ");
+                        WHERE id = :id";
+            $stmt = $this->database->prepare($sql);
             return $stmt->execute([
                 'id' => $entity->id,
                 'username' => $entity->username,
-                'password' => password_hash($entity->passowrd, PASSWORD_BCRYPT),
+                'password' => password_hash($entity->password, PASSWORD_BCRYPT),
                 'email' => $entity->email,
             ]);
         }
 
         // Creation
-        $stmt = $this->database->prepare("
-        INSERT INTO {$this->table} (username, password, email, created) 
-                VALUES (:username, :password, :email, NOW())
-                ");
+
+        $sql = "
+                    INSERT INTO {$this->table} (username, password, email, created) 
+                    VALUES (:username, :password, :email, NOW())";
+
+        $stmt = $this->database->prepare($sql);
         $result = $stmt->execute([
             'username' => $entity->username,
-            'password' => password_hash($entity->passowrd, PASSWORD_BCRYPT),
+            'password' => password_hash($entity->password, PASSWORD_BCRYPT),
             'email' => $entity->email,
         ]);
 
         if ($result) {
-            $entity->id = (int) $this->database->lastInsertId();
+            $entity->id = (int)$this->database->lastInsertId();
         }
 
         return $result;
+    }
+
+    public function isClient(int $id): bool
+    {
+        $sql = "SELECT 1 FROM client WHERE id_user = :id_user LIMIT 1";
+
+        $stmt = $this->database->prepare($sql);
+        $stmt->execute(['id_user' => $id]);
+
+        return (bool)$stmt->fetchColumn();
+    }
+
+    public function isAdministrator(int $id): bool
+    {
+        $sql = "SELECT 1 FROM administrator WHERE id_user = :id_user LIMIT 1";
+
+        $stmt = $this->database->prepare($sql);
+        $stmt->execute(['id_user' => $id]);
+
+        return (bool)$stmt->fetchColumn();
     }
 
 }
